5ed84680cc2e5a57b75840e27a2d6e7056013126
Nina <nina@ninabsavage.ca>
2025-07-23 12:28:51 -0400

    finalizing styling, header finalized.

--- react-version/src/sections/Header.jsx
+++ react-version/src/sections/Header.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { FaLaptopCode, FaBookOpen, FaHeadphones } from 'react-icons/fa';
 
 function Header() {
     return (
         <header>
             <div className='titles'>
-                <h1>Nina Bertrand</h1>
-                <h2>Junior Developer</h2>
+                <div className='title-main'>
+                    <h1>Nina Bertrand</h1>
+                    <h2>Junior Developer</h2>
+                </div>
 
                 <ul className='currently-list'>
-                    <li><strong>Working on:</strong> A new React-based component library</li>
-                    <li><strong>Reading:</strong> House of Salt & Sorrows by Erin A. Craig</li>
-                    <li><strong>Listening to:</strong> Lo-fi study beats.</li>
+                    <li>
+                        <FaLaptopCode />
+                        <span><strong>Working on:</strong> A new React-based component library</span>
+                    </li>
+                    <li>
+                        <FaBookOpen />
+                        <span><strong>Reading:</strong> House of Salt & Sorrows by Erin A. Craig</span>
+                    </li>
+                    <li>
+                        <FaHeadphones />
+                        <span><strong>Listening to:</strong> Lo-fi study beats.</span>
+                    </li>
                 </ul>
             </div>
+
             <nav className='page-menu'>
                 <ul className='menu'>
                     <li><a href='#about'>About</a></li>
