0305cf3a796ba1d2091d288de6ccf15afeadab3b
Nina <nina@ninabsavage.ca>
2025-08-06 11:35:17 -0400

    debugging

--- react-version/api/spotify-auth.js
+++ react-version/api/spotify-auth.js
@@ -1,16 +1,27 @@
 export default function handler(req, res) {
     const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-    const redirect_uri = 'https://nina-portfolio-ecru.vercel.app/api/spotify-callback';
 
+    if (!SPOTIFY_CLIENT_ID) {
+        return res.status(500).send('Missing Spotify Client ID');
+    }
+
+    const redirect_uri = 'https://nina-portfolio-ecru.vercel.app/api/spotify-callback';
     const scopes = 'user-read-recently-played';
 
-    const authURL = 'https://accounts.spotify.com/authorize' +
-        `client_id=${SPOTIFY_CLIENT_ID}` +
-        `&response_type=code` +
-        `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
-        `&scope=${encodeURIComponent(scopes)}` +
-        `&show_dialog=true`;
 
-    console.log('Redirecting to:', authURL)
+
+    const params = new URLSearchParams ({
+        client_id: SPOTIFY_CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: redirect_uri,
+        scope: scopes,
+        show_dialog: 'true'
+    });
+
+    const authURL = `https://accounts.spotify.com/authorize?${params.toString()}`;
+
+    console.log('Client ID:', SPOTIFY_CLIENT_ID)
+    console.log('Full auth URL:', authURL)
+    
     res.redirect(authURL);
 }
