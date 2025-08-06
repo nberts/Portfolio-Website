092e12f736d5fb97235b6f11a2c48382fd9348b4
Nina <nina@ninabsavage.ca>
2025-08-06 12:06:55 -0400

    debugging

--- react-version/api/status.js
+++ react-version/api/status.js
@@ -199,6 +199,7 @@ export default async function handler(req, res) {
   console.log('Final reading data:', reading);
   
   listeningTo = await getSpotifyRecentAlbum();
+  console.log('Final listening data', listeningTo)
 
   // Return all data
   res.status(200).json({
