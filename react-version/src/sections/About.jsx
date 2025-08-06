7df40257064ffeb9787777722b23f7dcc7227012
Nina <nina@ninabsavage.ca>
2025-08-06 18:19:08 -0400

    Correcting about me text

--- react-version/src/sections/About.jsx
+++ react-version/src/sections/About.jsx
@@ -11,13 +11,13 @@ function About() {
                 <h2>About Me</h2>
                 <p>
                 Hi there! I'm a Junior Developer based in Ottawa, Canada, with a passion for creating clean, 
-                intuitive web experiences. Coming from a background in public service and corporate security, 
-                I bring a unique perspective to development that values both functionality and user safety. I 
-                fell in love with frontend development and design, but I'm equally fascinated by understanding 
-                how everything works under the hood. My approach to coding is simple: make time when you can, 
-                take breaks when you need them, and always stay curious. Currently, I'm on a learning journey 
-                focused on building projects that interest me, and I share my experiences through my blog about 
-                coding as a parent. Because who says you can't debug code and life at the same time?
+                intuitive web experiences. Coming from a background in the public service, I bring a unique 
+                perspective to development that values both functionality and user safety. I fell in love with 
+                frontend development and design, but I'm equally fascinated by understanding how everything 
+                works under the hood. My approach to coding is simple: make time when you can, take breaks when 
+                you need them, and always stay curious. Currently, I'm on a learning journey focused on building
+                projects that interest me, and I share my experiences through my blog about coding as a parent 
+                with a career. Because who says you can't debug code and life at the same time?
                 </p>
                 <div className='about-grid'>
                     <div className='skill-box'>
